e391a6612cf193d26f7b4394d31c18d2dedeab04
jsk <kimd07@naver.com>
2026-04-03 13:06:16 +0900

    no message

---
+++ TMM/src/main/java/kdu/og/project/TmmApplication.java
@@ -0,0 +1,13 @@
+package kdu.og.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmmApplication.class, args);
+	}
+
+}
